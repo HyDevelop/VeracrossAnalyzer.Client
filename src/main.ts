c3419b97642b2cdb8371236fe4c2a767b6209fc9
Hykilpikonna <admin@moecraft.cc>
2019-08-16 16:56:11 +0800

    [U] Change language to english

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import Vue from 'vue';
+import ElementUI from 'element-ui';
+
 import App from './components/app/app.vue';
 
 Vue.config.productionTip = false;
+Vue.use(ElementUI, {locale: 'en-us'});
 
 new Vue({
   render: (h) => h(App),
