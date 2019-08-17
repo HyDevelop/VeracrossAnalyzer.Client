e51c0ac11532c04d8f3369c18d7100b4fd66390c
Hykilpikonna <admin@moecraft.cc>
2019-08-17 12:31:28 +0800

    [+] Import login comp. in app.ts

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -1,7 +1,8 @@
 import {Component, Vue} from 'vue-property-decorator';
+import Login from '@/components/login/login';
 
 @Component({
-    components: {},
+    components: {Login},
 })
 export default class App extends Vue
 {
