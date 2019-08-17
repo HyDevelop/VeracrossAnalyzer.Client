f0dff3c3f9d2cbce54e11948b45ee9d6915bba63
Hykilpikonna <admin@moecraft.cc>
2019-08-17 12:31:14 +0800

    [+] Add el-inputs to login comp.

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,4 +1,6 @@
 <template>
+    <el-input v-model="input" placeholder="School Username"></el-input>
+    <el-input placeholder="Veracross Password" v-model="input" show-password=""></el-input>
 </template>
 
 <script src="./login.ts" lang="ts"></script>
