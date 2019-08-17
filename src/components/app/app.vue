a257675a3605601a7c46c81a4540d6c9ad915c33
Hykilpikonna <admin@moecraft.cc>
2019-08-17 12:31:57 +0800

    [F] Fix invalid path

--- src/components/app/app.vue
+++ src/components/app/app.vue
@@ -5,5 +5,5 @@
   </div>
 </template>
 
-<script src="app.ts" lang="ts"></script>
-<style src="app.scss" lang="scss"></style>
+<script src="./app.ts" lang="ts"></script>
+<style src="./app.scss" lang="scss"></style>
