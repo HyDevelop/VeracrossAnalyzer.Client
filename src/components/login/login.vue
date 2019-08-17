0d194dea068a6ff55acadd363b82e46f6650c867
Hykilpikonna <admin@moecraft.cc>
2019-08-17 12:50:37 +0800

    [F] Add id to fix component import problem

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,6 +1,8 @@
 <template>
-    <el-input v-model="input" placeholder="School Username"></el-input>
-    <el-input placeholder="Veracross Password" v-model="input" show-password=""></el-input>
+    <div id="login">
+        <el-input v-model="input" placeholder="School Username"></el-input>
+        <el-input placeholder="Veracross Password" v-model="input" show-password=""></el-input>
+    </div>
 </template>
 
 <script src="./login.ts" lang="ts"></script>
