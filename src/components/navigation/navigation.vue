822a1fecc2acdfbd54b2c2c05db56d4287b027f5
Hykilpikonna <admin@moecraft.cc>
2019-08-21 15:40:06 +0800

    [+] Show courses with v-for

--- src/components/navigation/navigation.vue
+++ src/components/navigation/navigation.vue
@@ -4,6 +4,10 @@
 
             <el-menu-item index="overall">Overall</el-menu-item>
 
+            <el-submenu index="courses">
+                <template slot="title">Courses</template>
+                <el-menu-item v-for="course in courses" :index="`courses-${course}`">{{course}}</el-menu-item>
+            </el-submenu>
 
         </el-menu>
         <div class="line"></div>
