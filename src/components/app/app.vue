36b88571917719cc8bd33922a43e999b5b4caf32
Hykilpikonna <admin@moecraft.cc>
2019-08-21 15:41:05 +0800

    [+] Add navigation comp. to app.vue

--- src/components/app/app.vue
+++ src/components/app/app.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="app">
         <login v-if="showLogin" v-on:login:courses="onLogin"></login>
-
+        <navigation :courses="courses"></navigation>
     </div>
 </template>
 
