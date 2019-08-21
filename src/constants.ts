8cca38b55a65162c3345a81e2898a55cf0bc1f03
Hykilpikonna <admin@moecraft.cc>
2019-08-21 15:52:05 +0800

    [O] Fix TSLint: " should be '

--- src/constants.ts
+++ src/constants.ts
@@ -7,5 +7,5 @@ export default class Constants
      * Base url for api access
      * TODO: Use https for actual usage
      */
-    public static API_URL: string = "http://cn2.hydev.org:24021/api";
+    public static API_URL: string = 'http://cn2.hydev.org:24021/api';
 }
