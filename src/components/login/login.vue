2e697904c9642359a8c4e8e1942f9cd6c5a39bc7
Hykilpikonna <admin@moecraft.cc>
2019-08-21 14:19:39 +0800

    [+] Add an overlay to the login panel

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,10 +1,12 @@
 <template>
-    <div id="login">
-        <img alt="Vue logo" src="../../assets/logo.png">
-        <h1>Veracross Analyzer</h1>
-        <el-input v-model="username" placeholder="School Username"></el-input>
-        <el-input v-model="password" placeholder="Veracross Password" show-password=""></el-input>
-        <el-button plain type="primary" @click="onLoginClick">Login</el-button>
+    <div id="login" class="login-overlay">
+        <div class="login-panel">
+            <img alt="Vue logo" src="../../assets/logo.png">
+            <h1>Veracross Analyzer</h1>
+            <el-input v-model="username" placeholder="School Username"></el-input>
+            <el-input v-model="password" placeholder="Veracross Password" show-password=""></el-input>
+            <el-button plain type="primary" @click="onLoginClick">Login</el-button>
+        </div>
     </div>
 </template>
 
