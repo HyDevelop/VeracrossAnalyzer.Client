fdeab0f2c97bffbe11deee5bab3e1be97180a219
Hykilpikonna <admin@moecraft.cc>
2019-08-22 16:21:05 +0800

    [+] Create overall page element

---
+++ src/pages/overall/overall.ts
@@ -0,0 +1,8 @@
+import {Component, Vue} from 'vue-property-decorator';
+
+@Component({
+})
+export default class Overall extends Vue
+{
+
+}
---
+++ src/pages/overall/overall.vue
@@ -0,0 +1,8 @@
+<template>
+    <div id="overall">
+
+    </div>
+</template>
+
+<script src="overall.ts" lang="ts"></script>
+<style src="overall.scss" lang="scss"></style>
