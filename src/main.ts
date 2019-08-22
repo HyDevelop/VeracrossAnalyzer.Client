79b50cc59c597c7094da1200ad770228d6a292ff
Hykilpikonna <admin@moecraft.cc>
2019-08-22 17:00:41 +0800

    [+] Import VCharts with Vue.use()

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import Vue from 'vue';
 import ElementUI from 'element-ui';
+const VCharts = require('v-charts');
 
 import App from './components/app/app.vue';
 
 Vue.config.productionTip = false;
+
+// Use Element UI
 Vue.use(ElementUI, {locale: 'en-us'});
 
+// Use VCharts
+Vue.use(VCharts);
+
+// Init app
 new Vue({
   render: (h) => h(App),
 }).$mount('#app');
