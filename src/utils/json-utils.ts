1d96dcc0a81965efa578d31ce555b8800970fbaa
Hykilpikonna <admin@moecraft.cc>
2019-08-22 21:58:16 +0800

    [+] Create method to filter assignments

--- src/utils/json-utils.ts
+++ src/utils/json-utils.ts
@@ -16,4 +16,32 @@ interface Grade
 
 export default class JsonUtils
 {
+    /**
+     * This method filters the information provided in an assignments json.
+     *
+     * @param assignments Assignments object
+     * @returns Grade[] Filtered assignment grade object list
+     */
+    public static filterAssignments(assignments: any): Grade[]
+    {
+        let result: Grade[] = [];
+
+        assignments.assignments.forEach((assignment: any) =>
+        {
+            result.push(
+            {
+                type: assignment.assignment_type,
+                description: assignment.assignment_description,
+                date: assignment._date,
+                complete: assignment.complete_status,
+                include: assignment.include_in_calculated_grade == 1,
+                display: assignment.display_grade == 1,
+
+                scoreMax: assignment.maximum_score,
+                score: +assignment.raw_score
+            });
+        });
+
+        return result;
+    }
 }
