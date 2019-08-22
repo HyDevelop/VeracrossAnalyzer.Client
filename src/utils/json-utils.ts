811de8f5e2e18a57c169067ad1cd34d23d75f3d3
Hykilpikonna <admin@moecraft.cc>
2019-08-22 21:58:01 +0800

    [+] Create a grade interfacce

--- src/utils/json-utils.ts
+++ src/utils/json-utils.ts
@@ -1,3 +1,19 @@
+/**
+ * Objects of this interface represent assignment grades.
+ */
+interface Grade
+{
+    type: string,
+    description: string,
+    date: string,
+    complete: string,
+    include: boolean,
+    display: boolean,
+
+    scoreMax: number,
+    score: number
+}
+
 export default class JsonUtils
 {
 }
