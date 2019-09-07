446ed686bdbfa8e8accc470f1db07d1a29a7dd9f
Hykilpikonna <admin@moecraft.cc>
2019-09-07 10:05:53 -0400

    [+] Create splash ascii art

--- src/constants.ts
+++ src/constants.ts
@@ -8,4 +8,8 @@ export default class Constants
      * TODO: Use https for actual usage
      */
     public static API_URL: string = 'https://va.hydev.org/api';
+
+    public static SPLASH: string = '\\  /   /\\ ._  _.|  _  _ ._\n' +
+                                   ' \\/>< /--\\| |(_||\\//_(/_| \n' +
+                                   '    v0.1.1.0     /        '
 }
