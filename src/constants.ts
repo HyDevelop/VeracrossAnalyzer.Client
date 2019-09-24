50a5f9dcc5f205062c082c007ab740ea1784fb47
Hykilpikonna <admin@moecraft.cc>
2019-09-23 21:11:27 -0400

    [O] Remove todo that is already done

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,6 @@ export default class Constants
 {
     /**
      * Base url for api access
-     * TODO: Use https for actual usage
      */
     public static API_URL: string = 'https://va.hydev.org/api';
 
