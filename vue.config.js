9e8e44b91f62d8de3b88edb0ebc8dc95bacc7782
Hykilpikonna <admin@moecraft.cc>
2019-09-23 20:14:41 -0400

    [-] Remove public path

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,5 @@ module.exports =
     devServer:
     {
         disableHostCheck: true,
-    },
-    publicPath:
-        process.env.NODE_ENV === 'production' ? '/VeracrossAnalyzer.Client/' : '/'
+    }
 };
