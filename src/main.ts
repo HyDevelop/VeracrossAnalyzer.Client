b78c5b4f5b21bee7169daf472c2f0aeb30de5992
Hykilpikonna <admin@moecraft.cc>
2019-09-28 17:50:42 -0400

    [+] Import vue-echarts

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,29 @@
 import Vue from 'vue';
-import ElementUI from 'element-ui';
-
 import App from './components/app/app.vue';
-import VueCookies from 'vue-cookies';
 
-Vue.config.productionTip = false;
-
-// Use Element UI
+// Import Element UI
+import ElementUI from 'element-ui';
 Vue.use(ElementUI, {locale: 'en-us'});
 
-// Use Cookies
+// Import Cookies
+import VueCookies from 'vue-cookies';
 Vue.use(VueCookies);
 
+// Import ECharts
+// 在 webpack 环境下指向 components/ECharts.vue
+const ECharts = require('vue-echarts');
+Vue.component('v-chart', ECharts);
+
+// 手动引入 ECharts 各模块来减小打包体积
+import 'echarts/lib/chart/bar'
+import 'echarts/lib/component/tooltip'
+
+// Config
+Vue.config.productionTip = false;
+
 // Init app
-new Vue({
-  render: (h) => h(App),
-}).$mount('#app');
+new Vue(
+{
+    render: (h) => h(App),
+})
+.$mount('#app');
