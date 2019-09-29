cc4378d905cfee40eaa575c11dc86c8fc103dc35
Hykilpikonna <admin@moecraft.cc>
2019-09-29 12:44:55 -0400

    Revert "[+] Add vue-echarts transpile dependencies"
    This reverts commit a526946b83089aea79dfccbc2372c296c0560b3e.

--- vue.config.js
+++ vue.config.js
@@ -3,10 +3,5 @@ module.exports =
     devServer:
     {
         disableHostCheck: true,
-    },
-    transpileDependencies:
-    [
-        'vue-echarts',
-        'resize-detector'
-    ]
+    }
 };
