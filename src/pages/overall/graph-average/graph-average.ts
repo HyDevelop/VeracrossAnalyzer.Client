9731d4e380afc433adc0614f21126c451f9b45ba
Hykilpikonna <admin@moecraft.cc>
2019-09-29 16:13:52 -0400

    [+] GraphAverage: Map GPA and MaxGPA for each course

--- src/pages/overall/graph-average/graph-average.ts
+++ src/pages/overall/graph-average/graph-average.ts
@@ -1,5 +1,6 @@
 import {Component, Prop, Vue} from 'vue-property-decorator';
 import {Course} from '@/components/app/app';
+import {GPAUtils} from '@/utils/gpa-utils';
 
 @Component({
 })
@@ -21,6 +22,16 @@ export default class GraphAverage extends Vue
 
         // Make column list
         let columns = ['Course', 'GPA', 'MaxGPA'];
+
+        // Calculate GPA and MaxGPA for each course (rows)
+        let rows = courses.map(course =>
+        {
+            return {
+                'Course': course.name,
+                'GPA': GPAUtils.getGP(course, course.letterGrade),
+                'MaxGPA': GPAUtils.getGP(course, 'A+')
+            };
+        });
 
     }
 }
