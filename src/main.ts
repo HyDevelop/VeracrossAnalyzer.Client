74a033fb5d68b7b59ba10f55644a6bc4153fdae6
Hykilpikonna <admin@moecraft.cc>
2019-09-29 12:44:57 -0400

    Revert "[+] Import vue-echarts"
    This reverts commit b78c5b4f5b21bee7169daf472c2f0aeb30de5992.

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,18 @@
 import Vue from 'vue';
-import App from './components/app/app.vue';
-
-// Import Element UI
 import ElementUI from 'element-ui';
-Vue.use(ElementUI, {locale: 'en-us'});
 
-// Import Cookies
+import App from './components/app/app.vue';
 import VueCookies from 'vue-cookies';
-Vue.use(VueCookies);
 
-// Import ECharts
-// 在 webpack 环境下指向 components/ECharts.vue
-const ECharts = require('vue-echarts');
-Vue.component('v-chart', ECharts);
-
-// 手动引入 ECharts 各模块来减小打包体积
-import 'echarts/lib/chart/bar'
-import 'echarts/lib/component/tooltip'
-
-// Config
 Vue.config.productionTip = false;
 
+// Use Element UI
+Vue.use(ElementUI, {locale: 'en-us'});
+
+// Use Cookies
+Vue.use(VueCookies);
+
 // Init app
-new Vue(
-{
-    render: (h) => h(App),
-})
-.$mount('#app');
+new Vue({
+  render: (h) => h(App),
+}).$mount('#app');
