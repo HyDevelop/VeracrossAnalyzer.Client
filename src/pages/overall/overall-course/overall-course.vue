f13f3a155c4b9428119fab0cf65015e1b9d1da77
Hykilpikonna <admin@moecraft.cc>
2019-10-01 21:09:50 -0400

    [-] Remove the grid

--- src/pages/overall/overall-course/overall-course.vue
+++ src/pages/overall/overall-course/overall-course.vue
@@ -1,27 +1,22 @@
 <template>
     <div id="overall-course">
         <el-card class="course-card">
-            <el-row>
-                <el-col :span="6" class="course-col-name">
-                    <div class="course-name">
-                        {{course.name}}
-                    </div>
-                    <div class="course-teacher">
-                        {{course.teacherName}}
-                    </div>
-                </el-col>
-                <el-col :span="12">
-                    &nbsp;
-                </el-col>
-                <el-col :span="6" style="width: auto">
-                    <div class="course-letter-grade">
-                        {{course.letterGrade}}
-                    </div>
-                    <div class="course-numeric-grade">
-                        {{course.numericGrade}}
-                    </div>
-                </el-col>
-            </el-row>
+            <div class="course-col-name">
+                <div class="course-name">
+                    {{course.name}}
+                </div>
+                <div class="course-teacher">
+                    {{course.teacherName}}
+                </div>
+            </div>
+            <div class="course-col-grade">
+                <div class="course-letter-grade">
+                    {{course.letterGrade}} {{course.numericGrade}}
+                </div>
+                <div class="course-numeric-grade">
+
+                </div>
+            </div>
         </el-card>
     </div>
 </template>
