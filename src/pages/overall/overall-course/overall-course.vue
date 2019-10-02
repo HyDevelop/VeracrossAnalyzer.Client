19f03717bc37358d2422ee9f01dc5dbd8f645d85
Hykilpikonna <admin@moecraft.cc>
2019-10-02 08:12:53 -0400

    [O] Go back to using grid

--- src/pages/overall/overall-course/overall-course.vue
+++ src/pages/overall/overall-course/overall-course.vue
@@ -1,29 +1,34 @@
 <template>
     <div id="overall-course">
         <el-card class="course-card">
-            <div class="course-col-name">
-                <div class="course-name">
-                    {{course.name}}
-                </div>
-                <div class="course-teacher">
-                    {{course.teacherName}}
-                </div>
-            </div>
-            <div class="course-col-grade">
-                <div class="course-grade">
-                    <span class="letter">{{course.letterGrade}} </span>
-                    <span class="numeric">{{course.numericGrade.toFixed(2)}} </span>
-                    <span class="percent">%</span>
-                </div>
-                <!--div class="course-updates">
-                    <span class="notifications">
+            <el-row>
+                <el-col :span="6" class="course-col-name">
+                    <div class="course-name">
+                        {{course.name}}
+                    </div>
+                    <div class="course-teacher">
+                        {{course.teacherName}}
+                    </div>
+                </el-col>
+                <el-col :span="12">
+                    &nbsp;
+                </el-col>
+                <el-col :span="6" class="course-col-grade">
+                    <div class="course-grade">
+                        <span class="letter">{{course.letterGrade}} </span>
+                        <span class="numeric">{{course.numericGrade.toFixed(2)}} </span>
+                        <span class="percent">%</span>
+                    </div>
+                    <!--div class="course-updates">
+                        <span class="notifications">
 
-                    </span>
-                    <span class="">
+                        </span>
+                        <span class="">
 
-                    </span>
-                </div-->
-            </div>
+                        </span>
+                    </div-->
+                </el-col>
+            </el-row>
         </el-card>
     </div>
 </template>
