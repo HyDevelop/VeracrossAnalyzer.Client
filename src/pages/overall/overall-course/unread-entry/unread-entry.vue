882c7bb35eb311ddbc30f197328b939acc6549a3
Hykilpikonna <admin@moecraft.cc>
2019-10-05 16:53:51 -0400

    [-] Remove el-row and el-col

--- src/pages/overall/overall-course/unread-entry/unread-entry.vue
+++ src/pages/overall/overall-course/unread-entry/unread-entry.vue
@@ -1,19 +1,9 @@
 <template>
     <div class="unread-entry vertical-center">
-        <el-row class="unread-row">
-            <el-col :span="3">
-                {{formatDate(new Date(assignment.date))}}
-            </el-col>
-            <el-col :span="6">
-                {{assignment.type}}
-            </el-col>
-            <el-col :span="9">
-                {{assignment.description}}
-            </el-col>
-            <el-col :span="6">
-                {{assignment.score + '|' + assignment.scoreMax}}
-            </el-col>
-        </el-row>
+        <span>{{formatDate(new Date(assignment.date))}}</span>
+        <span>{{assignment.type}}</span>
+        <span>{{assignment.description}}</span>
+        <span>{{assignment.score + '|' + assignment.scoreMax}}</span>
     </div>
 </template>
 
