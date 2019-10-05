8a6af65786916c4a1c8dbb93f99c3a2eebfde8a4
Hykilpikonna <admin@moecraft.cc>
2019-10-05 16:50:32 -0400

    [+] Create method to format date with moment

--- src/pages/overall/overall-course/unread-entry/unread-entry.ts
+++ src/pages/overall/overall-course/unread-entry/unread-entry.ts
@@ -1,5 +1,6 @@
 import {Component, Prop, Vue} from 'vue-property-decorator';
 import {Assignment, Course} from '@/components/app/app';
+import moment from 'moment';
 
 @Component({
 })
@@ -7,4 +8,14 @@ export default class UnreadEntry extends Vue
 {
     // @ts-ignore
     @Prop({required: true}) assignment: Assignment;
+
+    /**
+     * Format a date to the displayed format
+     *
+     * @param date Date
+     */
+    formatDate(date: Date)
+    {
+        return moment(date).format('MMM Do');
+    }
 }
