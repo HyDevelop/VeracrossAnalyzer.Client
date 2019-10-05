54b70f6a1c5af3e298cffe7c9ebb9c9f6936d68b
Hykilpikonna <admin@moecraft.cc>
2019-10-05 13:39:08 -0400

    [F] Fix inversed condition

--- src/pages/overall/overall-course/overall-course.vue
+++ src/pages/overall/overall-course/overall-course.vue
@@ -24,7 +24,7 @@
                             {{countUnread()}}
                         </span>
                         <span class="unread-text">
-                            new update{{countUnread() >= 2 ? '' : 's'}}
+                            new update{{countUnread() >= 2 ? 's' : ''}}
                         </span>
                     </div>
                 </el-col>
