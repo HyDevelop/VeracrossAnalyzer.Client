69cd4ff050c49f51e0b4314697b45c272c5402e2
Hykilpikonna <admin@moecraft.cc>
2019-10-05 13:51:05 -0400

    [+] Use default theme in base div

--- src/components/app/app.vue
+++ src/components/app/app.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="app">
+    <div id="app" class="theme-default">
         <login v-if="showLogin" v-on:login:token="onLogin" :http="http"></login>
         <navigation :courses="filteredCourses"
                     v-on:sign-out="signOut()"
