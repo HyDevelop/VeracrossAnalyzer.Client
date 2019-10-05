9c54033343d4fcc36398fb1349a55bf12e7f4e84
Hykilpikonna <admin@moecraft.cc>
2019-10-05 15:11:18 -0400

    [+] Wrap course-card around a parent card

--- src/pages/overall/overall-course/overall-course.vue
+++ src/pages/overall/overall-course/overall-course.vue
@@ -1,34 +1,36 @@
 <template>
     <div id="overall-course">
-        <el-card class="course-card">
-            <el-row>
-                <el-col :span="6" class="course-col-name">
-                    <div class="course-name">
-                        {{course.name}}
-                    </div>
-                    <div class="course-teacher">
-                        {{course.teacherName}}
-                    </div>
-                </el-col>
-                <el-col :span="12">
-                    &nbsp;
-                </el-col>
-                <el-col :span="6" class="course-col-grade">
-                    <div class="course-grade">
-                        <span class="letter">{{course.letterGrade}} </span>
-                        <span class="numeric">{{course.numericGrade.toFixed(2)}}</span>
-                        <span class="percent">%</span>
-                    </div>
-                    <div class="course-updates" :class="countUnread() === 0 ? 'none' : 'unread'">
-                        <span class="unread-number">
-                            {{countUnread()}}
-                        </span>
-                        <span class="unread-text">
-                            new update{{countUnread() >= 2 ? 's' : ''}}
-                        </span>
-                    </div>
-                </el-col>
-            </el-row>
+        <el-card class="course-card parent">
+            <el-card class="course-card">
+                <el-row>
+                    <el-col :span="6" class="course-col-name">
+                        <div class="course-name">
+                            {{course.name}}
+                        </div>
+                        <div class="course-teacher">
+                            {{course.teacherName}}
+                        </div>
+                    </el-col>
+                    <el-col :span="12">
+                        &nbsp;
+                    </el-col>
+                    <el-col :span="6" class="course-col-grade">
+                        <div class="course-grade">
+                            <span class="letter">{{course.letterGrade}} </span>
+                            <span class="numeric">{{course.numericGrade.toFixed(2)}}</span>
+                            <span class="percent">%</span>
+                        </div>
+                        <div class="course-updates" :class="countUnread() === 0 ? 'none' : 'unread'">
+                            <span class="unread-number">
+                                {{countUnread()}}
+                            </span>
+                            <span class="unread-text">
+                                new update{{countUnread() >= 2 ? 's' : ''}}
+                            </span>
+                        </div>
+                    </el-col>
+                </el-row>
+            </el-card>
         </el-card>
     </div>
 </template>
