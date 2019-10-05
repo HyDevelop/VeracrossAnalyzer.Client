116592e4360f247541e611d8611f2490ace083fd
Hykilpikonna <admin@moecraft.cc>
2019-10-05 16:56:31 -0400

    [+] Add css selector classes to cols

--- src/pages/overall/overall-course/unread-entry/unread-entry.vue
+++ src/pages/overall/overall-course/unread-entry/unread-entry.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="unread-entry vertical-center">
         <el-row class="unread-row">
-            <el-col :span="3">
+            <el-col :span="3" class="date">
                 {{formatDate(new Date(assignment.date))}}
             </el-col>
-            <el-col :span="15">
+            <el-col :span="15" class="description">
                 {{assignment.type}}
                 {{assignment.description}}
             </el-col>
-            <el-col :span="6">
+            <el-col :span="6" class="grade">
                 {{assignment.score + '|' + assignment.scoreMax}}
             </el-col>
         </el-row>
