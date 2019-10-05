6b0e0fc8e806c1fc341c6437bff727da70e13534
Hykilpikonna <admin@moecraft.cc>
2019-10-05 15:42:38 -0400

    [U] Vertical center main cards

--- src/pages/overall/overall.vue
+++ src/pages/overall/overall.vue
@@ -2,7 +2,7 @@
     <div id="overall">
         <el-row>
             <el-col :span="4">
-                <el-card class="large gpa-card">
+                <el-card class="large gpa-card vertical-center">
                     <div style="padding: 14px;">
                         <span class="gpa header">GPA:</span>
                         <span class="gpa text">{{getGPA().gpa}}</span>
@@ -14,12 +14,12 @@
                 </el-card>
             </el-col>
             <el-col :span="14">
-                <el-card class="large overall-line-card">
+                <el-card class="large overall-line-card vertical-center">
                     <overall-line :courses="courses"></overall-line>
                 </el-card>
             </el-col>
             <el-col :span="6">
-                <el-card class="large overall-bar-card">
+                <el-card class="large overall-bar-card vertical-center">
                     <overall-bar :courses="courses"></overall-bar>
                 </el-card>
             </el-col>
