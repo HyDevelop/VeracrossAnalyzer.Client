0215be868159719c5e40b98f97894441cd892c62
Hykilpikonna <admin@moecraft.cc>
2019-10-05 20:01:46 -0400

    [O] Make days from now font smaller

--- src/pages/overall/overall-course/unread-entry/unread-entry.vue
+++ src/pages/overall/overall-course/unread-entry/unread-entry.vue
@@ -3,7 +3,7 @@
         <el-row class="unread-row">
             <el-col :span="2" class="date">
                 <span class="month">{{getMoment(assignment.date).format("MMM Do")}}</span>
-                <span class="now">{{getMoment(assignment.date).fromNow()}}</span>
+                <span class="now">({{getMoment(assignment.date).fromNow()}})</span>
             </el-col>
             <el-col :span="16" class="description">
                 <span class="type entry-box">{{assignment.type}}</span>
