cc23f8565a172c66aa503e8118f13496237ef280
Hykilpikonna <admin@moecraft.cc>
2019-10-13 19:42:17 -0400

    [+] Create loading overlay

---
+++ src/components/loading/loading.vue
@@ -0,0 +1,12 @@
+<template>
+    <div id="loading">
+        <div id="text">
+            {{text}}
+        </div>
+    </div>
+</template>
+
+<script src="./loading.ts" lang="ts"></script>
+
+<style scoped>
+</style>
