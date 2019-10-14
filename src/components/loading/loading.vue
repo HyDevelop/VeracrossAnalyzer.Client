755b61efe38fe6c7d545657cdbb37f3ac974e796
Hykilpikonna <admin@moecraft.cc>
2019-10-13 21:25:17 -0400

    [O] Only show spinner if not error

--- src/components/loading/loading.vue
+++ src/components/loading/loading.vue
@@ -3,7 +3,7 @@
         <div id="text">
             {{error ? 'Error' : 'Loading'}}
 
-            <div class="el-loading-spinner">
+            <div v-if="!error" class="el-loading-spinner">
                 <svg viewBox="25 25 50 50" class="circular">
                     <circle cx="50" cy="50" r="20" fill="none" class="path" />
                 </svg>
