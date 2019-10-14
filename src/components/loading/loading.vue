ccd32ed7e61adbfab16652aecf4e75fe7980e8a0
Hykilpikonna <admin@moecraft.cc>
2019-10-13 20:35:53 -0400

    [+] Add a loading spinner

--- src/components/loading/loading.vue
+++ src/components/loading/loading.vue
@@ -3,6 +3,12 @@
         <div id="text">
             Loading
 
+            <div class="el-loading-spinner">
+                <svg viewBox="25 25 50 50" class="circular">
+                    <circle cx="50" cy="50" r="20" fill="none" class="path" />
+                </svg>
+            </div>
+
             <div id="details">{{text}}</div>
         </div>
     </div>
