a5d261ac935b4addc0c2a06a5a24cd836f16c317
Hykilpikonna <admin@moecraft.cc>
2019-10-14 19:01:17 -0400

    [+] Use grid to display basic info

--- src/pages/course/course-page.vue
+++ src/pages/course/course-page.vue
@@ -1,5 +1,15 @@
 <template>
     <div id="course-page">
+        <el-row>
+            <el-col :span="8" id="course-description">
+                <div class="name">{{course.name}}</div>
+                <div class="teacher">{{course.teacherName}}</div>
+                <div class="level">{{course.level}}</div>
+                <div class="id">{{course.id}}</div>
+            </el-col>
+            <el-col :span="8"><div class="grid-content bg-purple-light"></div></el-col>
+            <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+        </el-row>
     </div>
 </template>
 
