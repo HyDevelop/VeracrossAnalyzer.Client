e95efbff590bab470d335bdcee182b3a154a5d41
Hykilpikonna <admin@moecraft.cc>
2019-10-13 20:00:10 -0400

    [O] Center text box vertically

--- src/components/loading/loading.vue
+++ src/components/loading/loading.vue
@@ -24,5 +24,11 @@
     #text
     {
         color: white;
+
+        margin: 0;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
     }
 </style>
