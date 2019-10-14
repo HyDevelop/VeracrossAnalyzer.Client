9b4f5291f0b198c113e602cd26a4a16def228784
Hykilpikonna <admin@moecraft.cc>
2019-10-14 17:57:04 -0400

    [O] Use v-bind.sync to sync variable value

--- src/components/app/app.vue
+++ src/components/app/app.vue
@@ -2,7 +2,7 @@
     <div id="app" class="theme-default">
         <login v-if="showLogin" v-on:login:token="onLogin"></login>
         <navigation :courses="filteredCourses"
-                    :activeIndex="selectedTab"
+                    :activeIndex.sync="selectedTab"
                     v-on:sign-out="signOut">
         </navigation>
 
