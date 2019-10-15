4e37ca4c44b446d22acc087be917824dd391c839
Hykilpikonna <admin@moecraft.cc>
2019-10-14 21:36:57 -0400

    [+] Add row on the expansion area

--- src/pages/course/course-page.vue
+++ src/pages/course/course-page.vue
@@ -3,7 +3,12 @@
         <course-head :course="course" :unread="countUnread()"></course-head>
 
         <div class="course-card-content expand notification">
-
+            <el-row>
+                <el-col :span="8" id="course-description">
+                </el-col>
+                <el-col :span="8"><div class="grid-content bg-purple-light"></div></el-col>
+                <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+            </el-row>
         </div>
     </el-card>
 </template>
