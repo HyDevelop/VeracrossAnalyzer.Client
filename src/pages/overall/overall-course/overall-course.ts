c4e1790444a6e5be6f041c679fcc42fe5fbd7a8f
Hykilpikonna <admin@moecraft.cc>
2019-10-14 20:27:25 -0400

    [+] Import course-head in overall-course

--- src/pages/overall/overall-course/overall-course.ts
+++ src/pages/overall/overall-course/overall-course.ts
@@ -3,9 +3,10 @@ import App, {Assignment, Course} from '@/components/app/app';
 import {GPAUtils} from '@/utils/gpa-utils';
 import Constants from '@/constants';
 import UnreadEntry from '@/pages/overall/overall-course/unread-entry/unread-entry';
+import CourseHead from '@/pages/overall/overall-course/course-head/course-head.vue';
 
 @Component({
-    components: {UnreadEntry}
+    components: {UnreadEntry, CourseHead}
 })
 export default class OverallCourse extends Vue
 {
