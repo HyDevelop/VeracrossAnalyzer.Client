5c530952d31f0e9905a9986c3fc80f6c84bde060
Hykilpikonna <admin@moecraft.cc>
2019-10-20 12:39:46 -0400

    [O] Add more colors

--- src/constants.ts
+++ src/constants.ts
@@ -26,14 +26,19 @@ export default class Constants
     {
         // Colors
         colors:
-        [
-            '#18cea5',
-            '#4fa8ed',
-            '#f9627b',
-            '#ffb075',
-            '#005c9c',
-            '#bcabe0',
-            '#d36e75',
+        ['#19d4ae',
+            '#5ab1ef',
+            '#fa6e86',
+            '#ffb980',
+            '#0067a6',
+            '#c4b4e4',
+            '#d87a80',
+            '#9cbbff',
+            '#d9d0c7',
+            '#87a997',
+            '#d49ea2',
+            '#5b4947',
+            '#7ba3a8',
             '#fc97af',
             '#919e8b',
             '#d7ab82',
