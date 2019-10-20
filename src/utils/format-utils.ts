0d86e461cdbb0f21ab73ef4ac7fb3f04df4f05a7
Hykilpikonna <admin@moecraft.cc>
2019-10-20 12:03:01 -0400

    [+] Create function to format chart date

--- src/utils/format-utils.ts
+++ src/utils/format-utils.ts
@@ -1,4 +1,18 @@
+import moment from 'moment';
+
 export class FormatUtils
 {
-    
+    /**
+     * Convert date format to yyyy-mm-dd
+     *
+     * @param _date Date
+     */
+    public static toChartDate(_date: string | Date)
+    {
+        // Convert to Date
+        let date: Date = _date instanceof Date ? _date : new Date(_date);
+
+        // Convert to yyyy-mm-dd
+        return moment(date).format('yyyy-mm-dd');
+    }
 }
