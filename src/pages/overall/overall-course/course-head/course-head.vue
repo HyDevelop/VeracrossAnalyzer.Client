85305f987f6260a169219bba166b271d1824d634
Hykilpikonna <admin@moecraft.cc>
2019-10-20 19:23:12 -0400

    [O] Optimize course-head grid distribution

--- src/pages/overall/overall-course/course-head/course-head.vue
+++ src/pages/overall/overall-course/course-head/course-head.vue
@@ -1,14 +1,11 @@
 <template>
     <div id="course-head" class="course-card-content main vertical-center">
         <el-row>
-            <el-col :span="6" class="course-col-name">
+            <el-col :span="12" class="course-col-name">
                 <div class="course-name">{{course.name}}</div>
                 <div class="course-teacher">{{course.teacherName}}</div>
             </el-col>
-            <el-col :span="12">
-                &nbsp;
-            </el-col>
-            <el-col :span="6" class="course-col-grade">
+            <el-col :span="12" class="course-col-grade">
                 <div class="course-grade">
                     <span class="letter">{{course.letterGrade}} </span>
                     <span class="numeric">{{course.numericGrade.toFixed(2)}}</span>
