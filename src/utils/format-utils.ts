f69e2617d4198fef7f95c45ceef795c0cd282715
Hykilpikonna <admin@moecraft.cc>
2019-10-20 21:55:13 -0400

    [+] Encapsulate toTitleCase()

--- src/utils/format-utils.ts
+++ src/utils/format-utils.ts
@@ -22,8 +22,19 @@ export class FormatUtils
      * @param str String
      * @param length Max length
      */
-    static limit(str: string, length: number): string
+    public static limit(str: string, length: number): string
     {
         return str.length <= length ? str : str.substr(0, length - 2) + '...'
+    }
+
+    /**
+     * To Title Case
+     *
+     * @param str oRigInAL sTrING
+     * @return string Original String
+     */
+    public static toTitleCase(str: string)
+    {
+        return str.replace(/\w\S*/g, s => s.charAt(0).toUpperCase() + s.substr(1).toLowerCase())
     }
 }
