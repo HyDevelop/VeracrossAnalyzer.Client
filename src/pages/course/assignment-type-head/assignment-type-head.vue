202feb12d7f6eb8f6616aecd8399f8aa3e6da627
Hykilpikonna <admin@moecraft.cc>
2019-11-05 17:22:13 -0500

    [+] Create assignment type head vue comp

---
+++ src/pages/course/assignment-type-head/assignment-type-head.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="assignment-type-head">
+        <el-card>
+
+        </el-card>
+    </div>
+</template>
+
+<script lang="ts">
+    export default class AssignmentTypeHead
+    {
+
+    }
+</script>
+
+<style scoped>
+
+</style>
