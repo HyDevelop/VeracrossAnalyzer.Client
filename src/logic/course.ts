17262e12af95313be039b6713154e8fa81ca9701
Hykilpikonna <admin@moecraft.cc>
2019-11-06 18:05:53 -0500

    [+] Create constructor

--- src/logic/course.ts
+++ src/logic/course.ts
@@ -2,18 +2,18 @@ import {Assignment} from '@/components/app/app';
 
 export default class Course
 {
-    assignmentsId: number;
     id: number;
+    assignmentsId: number;
     name: string;
     teacherName: string;
     status: string;
+    assignments: Assignment[];
 
     letterGrade?: string;
     numericGrade?: number;
 
     level: string;
     scaleUp: number;
-    assignments: Assignment[];
 
     grading:
     {
@@ -26,4 +26,27 @@ export default class Course
         termAssignments: Assignment[][]
         allYearGrade: number
     };
+
+    /**
+     * Construct a course with a course json object
+     *
+     * @param courseJson Course json object
+     */
+    constructor(courseJson: any)
+    {
+        this.id = courseJson.id;
+        this.assignmentsId = courseJson.assignmentsId;
+        this.name = courseJson.name;
+        this.teacherName = courseJson.teacherName;
+        this.status = courseJson.status;
+        this.assignments = courseJson.assignments;
+
+        this.letterGrade = courseJson.letterGrade;
+        this.numericGrade = courseJson.numericGrade;
+
+        this.level = courseJson.level;
+        this.scaleUp = courseJson.scaleUp;
+
+        this.grading = courseJson.grading;
+    }
 }
