9fdb7461baac7d90002eafb181e7bd5193d0ae4f
Hykilpikonna <admin@moecraft.cc>
2019-11-10 13:28:47 -0500

    [S] Create dark css

--- src/components/app/app.vue
+++ src/components/app/app.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="app" class="theme-default">
+    <div id="app" class="theme-default dark">
         <login v-if="showLogin" v-on:login:token="onLogin"></login>
         <navigation :courses="filteredCourses"
                     :activeIndex.sync="selectedTab"
