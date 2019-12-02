991580473da0dc9e8a876b2aa0119cf976a49ba3
Hykilpikonna <admin@moecraft.cc>
2019-12-02 14:17:04 -0500

    [+] Create constructor for it

--- src/logic/login-user.ts
+++ src/logic/login-user.ts
@@ -16,4 +16,23 @@ export default class LoginUser
     birthday: string;
     avatarUrl: string;
     token: string;
+
+    constructor(json: any)
+    {
+        this.id = json.id;
+        this.schoolPersonPk = json.schoolPersonPk;
+        this.username = json.username;
+        this.lastLogin = new Date(json.lastLogin);
+        this.firstLogin = new Date(json.firstLogin);
+        this.firstName = json.firstName;
+        this.lastName = json.lastName;
+        this.nickname = json.nickname;
+        this.graduationYear = json.graduationYear;
+        this.groups = json.groups;
+        this.emails = json.emails.split('|');
+        this.classes = json.classes.split('|');
+        this.birthday = json.birthday;
+        this.avatarUrl = json.avatarUrl;
+        this.token = json.token;
+    }
 }
