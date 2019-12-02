c6ca171101f659279d467c263d684da14a726f32
Hykilpikonna <admin@moecraft.cc>
2019-12-02 14:16:54 -0500

    [+] Create login-user class

---
+++ src/logic/login-user.ts
@@ -0,0 +1,19 @@
+
+export default class LoginUser
+{
+    id: number;
+    schoolPersonPk: number;
+    username: string;
+    lastLogin: Date;
+    firstLogin: Date;
+    firstName: string;
+    lastName: string;
+    nickname: string;
+    graduationYear: string;
+    groups: string;
+    emails: string[];
+    classes: string[];
+    birthday: string;
+    avatarUrl: string;
+    token: string;
+}
