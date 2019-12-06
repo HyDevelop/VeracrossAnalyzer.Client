698db97eb14b317d1e36c971bef50e45ff183f7e
Hykilpikonna <admin@moecraft.cc>
2019-12-05 23:19:19 -0500

    [+] Create get() method for cache

--- src/logic/utils/cache-utils.ts
+++ src/logic/utils/cache-utils.ts
@@ -2,4 +2,19 @@
 export default class Cache
 {
     map: Map<string, any> = new Map();
+
+    /**
+     * Get a cached value, or if not cached, cache it.
+     *
+     * @param name Name of the cached value
+     * @param callback Callback function
+     */
+    public get(name: string, callback: () => any)
+    {
+        if (!this.map.has(name))
+        {
+            this.map.set(name, callback());
+        }
+        return this.map.get(name);
+    }
 }
