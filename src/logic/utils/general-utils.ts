2d119ce53177a36a12516680818e1c9b04478e1a
Hykilpikonna <admin@moecraft.cc>
2019-12-07 14:05:09 -0500

    [F] Fix not found problem

--- src/logic/utils/general-utils.ts
+++ src/logic/utils/general-utils.ts
@@ -2,6 +2,6 @@
 export function findLastIndex<T>(array: T[], callback: (v: T) => boolean): number
 {
     let arr2 = array.slice().reverse();
-    let result = arr2.length - arr2.findIndex(callback);
-    return result == -1 ? -1 : result - 1;
+    let result = arr2.findIndex(callback);
+    return result == -1 ? -1 : arr2.length - result - 1;
 }
