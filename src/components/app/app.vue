d56d9857343e1281f7cb70c470c1611ff70e1baf
Hykilpikonna <admin@moecraft.cc>
2020-01-05 16:22:56 -0500

    [+] Display content according to staticpage

--- src/components/app/app.vue
+++ src/components/app/app.vue
@@ -1,23 +1,27 @@
 <template>
     <div id="app" class="theme-default">
-        <login v-if="showLogin" v-on:login:user="onLogin"/>
-        <navigation v-if="user != null"
-                    :courses="gradedCourses"
-                    :user="user"
-                    :nav="nav"
-                    @sign-out="signOut" @select-time="selectTime">
-        </navigation>
+        <div id="app-inner" v-if="staticPage === ''">
+            <login v-if="showLogin" v-on:login:user="onLogin"/>
+            <navigation v-if="user != null"
+                        :courses="gradedCourses"
+                        :user="user"
+                        :nav="nav"
+                        @sign-out="signOut" @select-time="selectTime">
+            </navigation>
 
-        <div id="app-content" v-if="assignmentsReady && loading === ''">
-            <overall v-if="nav.id === 'overall'"
-                     :courses="gradedCourses">
-            </overall>
-            <course-page v-if="nav.id === 'course'"
-                         :course="gradedCourses.find(c => +c.id === +nav.info.id)">
-            </course-page>
+            <div id="app-content" v-if="assignmentsReady && loading === ''">
+                <overall v-if="nav.id === 'overall'"
+                         :courses="gradedCourses">
+                </overall>
+                <course-page v-if="nav.id === 'course'"
+                             :course="gradedCourses.find(c => +c.id === +nav.info.id)">
+                </course-page>
+            </div>
+
+            <loading v-if="loading !== ''" :text="loading" :error="loadingError"/>
         </div>
 
-        <loading v-if="loading !== ''" :text="loading" :error="loadingError"/>
+        <Info v-if="staticPage === 'info'"/>
     </div>
 </template>
 
