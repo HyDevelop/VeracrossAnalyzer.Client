d1cfc69b5cd5abd8f56dc578d938d8745a1e87a5
Hykilpikonna <admin@moecraft.cc>
2020-04-19 19:52:01 -0400

    [+] Create SearchSettings vue

---
+++ src/pages/course-selection/search-settings/search-settings.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="settings">
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component, Vue} from 'vue-property-decorator'
+
+    @Component
+    export default class SearchSettings extends Vue
+    {
+
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
