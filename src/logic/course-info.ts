0306585cf8c408517f97d4b973206117bacda037
Hykilpikonna <admin@moecraft.cc>
2020-04-19 19:18:30 -0400

    [+] Create courseInfo class

--- src/logic/course-info.ts
+++ src/logic/course-info.ts
@@ -29,11 +29,16 @@ export default class CourseInfo
 
         this.courseCount = this.courseIds.length;
         this.gradeLevels = [];
-        this.uniqueName = this.name
+        this.uniqueName = CourseInfo.toUniqueName(this.name);
+        this.enrollments = 0;
+    }
+
+    static toUniqueName(name: string)
+    {
+        return name
             .replace(/( Semester| Full Year|)/g, '')
             .replace(/( Accelerated| Honors| College Prep|)/g, '')
-            .replace(/( A| Acc| CP| H| \(.*\))$/g, '')
-        this.enrollments = 0;
+            .replace(/( A| Acc| CP| H| \(.*\))$/g, '');
     }
 }
 
@@ -42,4 +47,29 @@ export interface UniqueCourse
     name: string
     courses: CourseInfo[]
     enrollments: number
+}
+
+export class ClassInfo
+{
+    id: number
+    name: string
+    teacher: string
+    level: string
+
+    uniqueName: string
+
+    /**
+     * Construct with a json object
+     *
+     * @param json
+     */
+    constructor(json: any)
+    {
+        this.id = json.id;
+        this.name = json.name.trim().replace('&amp;', '&').replace('&quot;', '"')
+        this.teacher = json.teacher
+        this.level = json.level;
+
+        this.uniqueName = CourseInfo.toUniqueName(this.name);
+    }
 }
