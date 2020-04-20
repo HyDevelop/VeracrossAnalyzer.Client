cc039655ae6cd05e75bff8dd28e3574f5dfae74a
Hykilpikonna <admin@moecraft.cc>
2020-04-20 17:59:09 -0400

    [+] Encapsulate isNumeric()

--- src/logic/utils/general-utils.ts
+++ src/logic/utils/general-utils.ts
@@ -5,3 +5,8 @@ export function findLastIndex<T>(array: T[], callback: (v: T) => boolean): numbe
     let result = arr2.findIndex(callback);
     return result == -1 ? -1 : arr2.length - result - 1;
 }
+
+export function isNumeric(str: string)
+{
+    return !isNaN(parseFloat(str)) && isFinite(+str);
+}
