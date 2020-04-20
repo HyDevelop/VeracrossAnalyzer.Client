d821be41dfab2016ae614a1e6568c0b4f51cbd60
Hykilpikonna <admin@moecraft.cc>
2020-04-20 16:57:05 -0400

    [+] Add courseInfo property to course-detail page

--- src/pages/course-selection/pages/course-detail.vue
+++ src/pages/course-selection/pages/course-detail.vue
@@ -5,15 +5,17 @@
 </template>
 
 <script lang="ts">
-    import {Component, Vue} from 'vue-property-decorator'
+    import {Component, Prop, Vue} from 'vue-property-decorator'
+    import CourseInfo from '@/logic/course-info';
 
     @Component
     export default class CourseDetail extends Vue
     {
-
+        @Prop({required: true}) courseInfo: CourseInfo;
     }
 </script>
 
+<style src="./pages.scss" lang="scss" scoped/>
 <style lang="scss" scoped>
 
 </style>
