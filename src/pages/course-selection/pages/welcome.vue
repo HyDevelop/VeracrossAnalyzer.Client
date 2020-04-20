587f1d4a9375ad8f99c3ceedc79d4d0b153429de
Hykilpikonna <admin@moecraft.cc>
2020-04-20 15:19:13 -0400

    [+] Create welcome component for course-selection

---
+++ src/pages/course-selection/pages/welcome.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="welcome">
+
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component, Vue} from 'vue-property-decorator'
+
+    @Component
+    export default class Welcome extends Vue
+    {
+
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
