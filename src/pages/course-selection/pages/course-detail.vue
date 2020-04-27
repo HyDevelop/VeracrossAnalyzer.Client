c860ffd31583fae0e927440e4695fd4944650b7e
Hykilpikonna <admin@moecraft.cc>
2020-04-27 18:16:37 -0400

    [S] Adjust margin under title

--- src/pages/course-selection/pages/course-detail.vue
+++ src/pages/course-selection/pages/course-detail.vue
@@ -168,6 +168,7 @@
                 white-space: nowrap;
                 overflow: hidden;
                 text-overflow: ellipsis;
+                margin-bottom: -10px;
             }
 
             .subtitle
