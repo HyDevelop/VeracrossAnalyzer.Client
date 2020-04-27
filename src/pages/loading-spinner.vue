b664674a06868d6dc59d14775fb9dd915d08ea04
Hykilpikonna <admin@moecraft.cc>
2020-04-27 05:04:10 -0400

    [+] Create a loading spinner component

---
+++ src/pages/loading-spinner.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="el-loading-spinner">
+        <svg viewBox="25 25 50 50" class="circular" :style="{width: size + 'px', height: size + 'px'}">
+            <circle cx="50" cy="50" r="20" fill="none" class="path"/>
+        </svg>
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component, Prop, Vue} from 'vue-property-decorator'
+
+    @Component
+    export default class LoadingSpinner extends Vue
+    {
+        @Prop({default: 42}) size: number
+    }
+</script>
+
+<style lang="scss" scoped></style>
