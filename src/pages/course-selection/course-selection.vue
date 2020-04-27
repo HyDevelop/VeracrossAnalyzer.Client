8a7007c7b7c59ff4b09895aa0ae84df6e10f9767
Hykilpikonna <admin@moecraft.cc>
2020-04-27 05:10:52 -0400

    [U] Use LoadingSpinner

--- src/pages/course-selection/course-selection.vue
+++ src/pages/course-selection/course-selection.vue
@@ -1,11 +1,7 @@
 <template>
     <div id="course-selection">
         <div v-if="loading" class="loading vertical-center" style="height: 100%">
-            <div class="el-loading-spinner">
-                <svg viewBox="25 25 50 50" class="circular">
-                    <circle cx="50" cy="50" r="20" fill="none" class="path"/>
-                </svg>
-            </div>
+            <LoadingSpinner/>
         </div>
 
         <el-row v-else>
