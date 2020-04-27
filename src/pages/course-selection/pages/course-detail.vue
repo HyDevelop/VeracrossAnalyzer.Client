9665b3a79307db33bd73e6e75ddca07d26f1e856
Hykilpikonna <admin@moecraft.cc>
2020-04-27 17:24:17 -0400

    [S] Adjust numeric rating margins

--- src/pages/course-selection/pages/course-detail.vue
+++ src/pages/course-selection/pages/course-detail.vue
@@ -98,6 +98,11 @@
             display: inline-block;
             margin-right: 10px;
         }
+
+        .numeric-rating
+        {
+            margin-right: 10px;
+        }
     }
 
     .float-left
