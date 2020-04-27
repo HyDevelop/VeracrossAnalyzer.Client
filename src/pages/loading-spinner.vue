606732c987754dbb15f3dbbcbefd492b7112d8d5
Hykilpikonna <admin@moecraft.cc>
2020-04-27 05:08:45 -0400

    [+] Add centered property to loading spinner

--- src/pages/loading-spinner.vue
+++ src/pages/loading-spinner.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="el-loading-spinner">
+    <div class="el-loading-spinner" :class="{'not-centered': !centered}">
         <svg viewBox="25 25 50 50" class="circular" :style="{width: size + 'px', height: size + 'px'}">
             <circle cx="50" cy="50" r="20" fill="none" class="path"/>
         </svg>
@@ -12,8 +12,18 @@
     @Component
     export default class LoadingSpinner extends Vue
     {
-        @Prop({default: 42}) size: number
+        @Prop({default: "42"}) size: string
+        @Prop({default: true}) centered: string
     }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+    .not-centered
+    {
+        top: unset;
+        margin-top: unset;
+        width: unset;
+        text-align: unset;
+        position: unset;
+    }
+</style>
